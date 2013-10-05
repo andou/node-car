aa0e6bbc31a9d7cf3c3229f979549c09875feb94
andou <antonio.pastorino@gmail.com>
2013-10-05 12:50:44 +0200

    Modification to the routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,6 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('model', { title: 'pippo', rows : ['aaa','bbb'] });
-};
-
-
 exports.carmodels = function(req, res){
 	
 	res.send(req.query.name);
