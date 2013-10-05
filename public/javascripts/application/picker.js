56b611de18b662388dba46146b5ec3b24653a901
janakp <j.patel@replyltd.co.uk>
2013-10-05 23:36:09 +0100

    resolved conflict

--- public/javascripts/application/picker.js
+++ public/javascripts/application/picker.js
@@ -1,12 +1,18 @@
 (function ($) {
-	$(".picker .items").height(($(window).height() - 200) + 'px');
+	$(".picker .items").height(($(window).height() - 100) + 'px');
 	
 	$("#content .picker .items .menu_left .item").bind('click touchend', function(){
 		$("#itemID").val($(this).find('input[name="id"]').val());
-		console.log($(this).find('input[name="id"]').val());
-		
+
 		if(typeof $(this).find('input[name="mainImage"]') != 'undefined') {
 			$(".preview .image img").attr('src', $(this).find('input[name="mainImage"]').val());
+		}
+		if(typeof $(this).find('input[name="runningCost"]') != 'undefined') { 
+			console.log($(this).find('input[name="runningCost"]').val());
+			var val = $(this).find('input[name="runningCost"]').val();
+			$('.running_total').each(function(){
+				$(this).find('.cost').html(val);
+			});
 		}
 	});
 }($));
