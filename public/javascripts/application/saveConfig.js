4a45dfc5416c0bdb5f9ed07babbcbf03798befbc
janakp <j.patel@replyltd.co.uk>
2013-10-06 09:04:06 +0100

    comment

---
+++ public/javascripts/application/saveConfig.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$("#save").bind('click touch', function(){
+		var name=prompt("Please enter a label to save your configuration","i.e. The best car in the world");
+	    if (name!=null){
+	    	
+	    }
+	});
+});
